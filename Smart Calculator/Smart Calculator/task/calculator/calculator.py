26f52a4eeceabd88296cdf27d7ff38fe036a7af0
Fabel <38182176+FabelVlad@users.noreply.github.com>
2020-05-09 01:46:42 +0300

    Update calculator.py

--- Smart Calculator/Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/Smart Calculator/task/calculator/calculator.py
@@ -4,10 +4,10 @@ class Calculator:
     def __init__(self):
         self.var_dict = {}
         self.math_sym_dict = {'-': lambda x, y: x - y,
-                            '+': lambda x, y: x + y,
-                            '*': lambda x, y: x * y,
-                            '/': lambda x, y: x / y,
-                            '^': lambda x, y: x ^ y}
+                              '+': lambda x, y: x + y,
+                              '*': lambda x, y: x * y,
+                              '/': lambda x, y: x / y,
+                              '^': lambda x, y: x ^ y}
         self.command_dict = {'/exit': lambda: exit(),
                              '/help': lambda: '/-/'}
         self.priority_dict = {'-': 1, '+': 1, '*': 2, '/': 2, '^': 3, '(': 4, ')': 0}
@@ -18,9 +18,12 @@ class Calculator:
         except ZeroDivisionError:
             pass
 
-    def _check_for_double_symbols(self, expr: str):
+    def _check_for_invalid_double_symbols(self, expr: str):
         for s in list(self.math_sym_dict.keys())[2:]:
-            if s in expr: return False
+            for i in range(len(expr)):
+                if s == expr[i] and s == expr[i - 1]:
+                    print(s)
+                    return False
         return True  # todo review method, can be static?? Yes
 
     def _get_variable(self, key: str) -> int:
@@ -42,13 +45,29 @@ class Calculator:
         except KeyError:
             print('Unknown command')
 
-    def _replace_element(self, expr: str):
-        expr_list = expr.replace(' ', '')
+    def _convert_double_symbols(self, expr: str):
+        chars = list(expr.replace(' ', ''))
+        count = 1
+        for i in range(len(chars) - 1):
+            if chars[i] == chars[i + 1]:
+                if chars[i] == list(self.math_sym_dict.keys())[1]:
+                    chars[i] = ''
+                elif chars[i] == list(self.math_sym_dict.keys())[0]:
+                    count += 1
+                    chars[i] = ''
+            elif count > 1:
+                if count % 2 == 0:
+                    chars[i] = '+'
+                else:
+                    chars[i] = '-'
+                count = 1
+        return ''.join(chars)
 
-    def validate_math_expression(self, expr: str) -> list:
-        if self._check_for_double_symbols(expr) and expr.count('(') == expr.count(')'):
-            # replace expr like ++ or ---- on equal
 
+    def validate_math_expression(self, expr: str) -> list:  # or None
+        if self._check_for_invalid_double_symbols(expr) and expr.count('(') == expr.count(')'):
+            # convert expr like ++ or ---- on equal
+            expr = self._convert_double_symbols(expr)
             return calculator._make_elements_list(expr)
         else:
             print('Invalid expression')
@@ -56,18 +75,51 @@ class Calculator:
     def view(self, key: str):
         print(self._get_variable(key))
 
-
+    def calculate(self, expression: list):
+        num_stack = []
+        symbol_stack = []
+        print(expression)
+        for el in expression:
+            if el == '(':
+                symbol_stack.append(el)
+            elif el == ')':
+                while True:
+                    if symbol_stack[-1] == '(':
+                        symbol_stack.pop()
+                        break
+                    else:
+                        num_stack.append(self._calc(num_stack.pop(), num_stack.pop(), symbol_stack.pop()))
+            elif el in self.priority_dict:
+                if symbol_stack == [] or symbol_stack[-1] == '(' or self.priority_dict[el] > self.priority_dict[symbol_stack[-1]]:
+                    symbol_stack.append(el)
+                elif self.priority_dict[symbol_stack[-1]] >= self.priority_dict[el]:
+                    num_stack.append(self._calc(num_stack.pop(), num_stack.pop(), symbol_stack.pop()))
+                    while True:
+                        if not symbol_stack or symbol_stack[-1] == '(' or self.priority_dict[symbol_stack[-1]] < \
+                                self.priority_dict[
+                                    el]:
+                            symbol_stack.append(el)
+                            break
+                        elif self.priority_dict[symbol_stack[-1]] >= self.priority_dict[el]:
+                            num_stack.append(self._calc(num_stack.pop(), num_stack.pop(), symbol_stack.pop()))
+                else:
+                    num_stack.append(self._calc(num_stack.pop(), num_stack.pop(), el))
+            else:
+                num_stack.append(int(el))
+        if num_stack:
+            for el in num_stack:
+                num_stack.append(self._calc(num_stack.pop(), num_stack.pop(), symbol_stack.pop()))
+        print(num_stack[0])
 
 
 if __name__ == '__main__':
     calculator = Calculator()
     while True:
         expression = input()
-        if expression in calculator.math_sym_dict and len(expression) > 1:
+        if expression in calculator.command_dict:
             calculator.command(expression)
         elif '=' in expression:
-            expr_list = expression.split()
-            if len(expr_list) < 4:
+            if len(expression.split()) < 4:
                 expr_list = expression.replace(' ', '').split('=')
                 if expr_list[0].isalpha():
                     if expr_list[1].isnumeric():
@@ -83,5 +135,5 @@ if __name__ == '__main__':
         elif expression.isalpha():
             calculator.view(expression)
         else:
-            if calculator.validate_math_expression(expression):
-                print(calculator.validate_math_expression(expression))  # calc will be receive list of elements of expr
+            clear_expression = calculator.validate_math_expression(expression)
+            calculator.calculate(clear_expression)
